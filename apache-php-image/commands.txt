150a61a560e294328a5801f9dfde3a3673bd9686
Mario Tomic <mario-tomic@hotmail.com>
2021-05-23 18:27:55 +0200

    Labo partie 2b

--- apache-php-image/commands.txt
+++
@@ -1,2 +0,0 @@
-docker build -t res/apache_php .
-docker run -p 9090:80 res/apache_php
--- express-image/src/index.js
+++ express-image/src/index.js
@@ -1,4 +1,43 @@
 var Chance = require('chance');
 var chance = new Chance();
+var Express = require('express');
+var app = new Express();
 
-console.log("Bonjour " + chance.name());
+
+app.get('/', function(req, res) {
+	res.send( generateStudents() );
+});
+
+
+app.listen(3000, function () {
+	console.log('Accepting HTTP requests on port 3000.');
+});
+
+function generateStudents() {
+	var numberOfPets = chance.integer({
+		min: 0,
+		max: 10
+	});
+	console.log(numberOfPets);
+	var pets = [];
+	for(var i = 0; i < numberOfPets; i++) {
+		var gender = chance.gender();
+		var birthYear = chance.year({
+			min: 2000,
+			max: 2019
+		});
+		pets.push({
+			firstName : chance.first({
+				gender: gender
+			}),
+			gender: gender,
+			birthday: chance.birthday({
+				year: birthYear
+			}),
+			animal: chance.animal()
+		});
+	};
+	
+	console.log(pets);
+	return pets;
+};
